1b518387c24a8f9c5dcb3fc1abf7fc954daf5695
cbodden <cesar@pissedoffadmins.com>
2013-04-02 20:22:30 -0400

    Initial color grid commit.

---
+++ colorgrid.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+function cgrid() {
+    for x in $(seq -w 029 047); do
+        echo -n ${x}\;XXX "|"
+        for y in $(seq -w 029 047); do
+            if [ "$x" == "029" ]; then
+                echo -en "\033[4m" XXX\;${y} "\033[0m"
+            else
+                echo -en "\033[${x};${y}m" ${x}\;${y} "\033[0m"
+            fi
+        done
+        printf "\n"
+    done
+}
+
+function pgrid() {
+    for x in $(seq -w 089 107); do
+        echo -n ${x}\;XXX "|"
+        for y in $(seq -w 089 107); do
+            if [ "$x" == "089" ]; then
+                echo -en "\033[4m" XXX\;${y} "\033[0m"
+            else
+                echo -en "\033[${x};${y}m" ${x}\;${y} "\033[0m"
+            fi
+        done
+        printf "\n"
+    done
+}
+
+case $1 in
+    'normal'|'regular'|'standard')
+        cgrid
+    ;;
+
+    'pastel'|'pastels')
+        pgrid
+    ;;
+
+    'all')
+        cgrid
+        printf "\n"
+        pgrid
+        printf "\n"
+        printf "\n"
+    ;;
+
+'help'|*) echo "usage: $0 <normal|pastel|all>"
+esac
